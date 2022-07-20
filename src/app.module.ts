72836540b6da5dcfc9f2e8a92c49032395126acd
igorRL <igor-rl@hotmail.com>
2022-07-20 09:14:19 -0300

    comexão com o banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InstituitionsModule } from './instituitions/instituitions.module';
@@ -8,7 +10,29 @@ import { BancobrasilBoletosModule } from './bancobrasil-boletos/bancobrasil-bole
 import { BancobrasilBoletoRegistrosModule } from './bancobrasil-boleto-registros/bancobrasil-boleto-registros.module';
 
 @Module({
-  imports: [InstituitionsModule, MetodosModule, AccountsModule,BancobrasilBoletosModule, BancobrasilBoletoRegistrosModule],
+  imports: 
+  [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+        // @ts-ignore
+        type: process.env.TYPEORM_CONNECTION,
+        host: process.env.TYPEORM_HOST,
+        port: parseInt(process.env.TYPEORM_PORT),
+        username: process.env.TYPEORM_USERNAME,
+        password: process.env.TYPEORM_PASSWORD,
+        database: process.env.TYPEORM_DATABASE,
+        entities: [process.env.TYPEORM_ENTITIES],
+        migration: [process.env.TYPEORM_MIGRATIONS],
+        autoLoadEntities: true,
+        keepConnectionAlive: true,
+        debug: true
+    }),
+    InstituitionsModule,
+    MetodosModule,
+    AccountsModule,
+    BancobrasilBoletosModule,
+    BancobrasilBoletoRegistrosModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
