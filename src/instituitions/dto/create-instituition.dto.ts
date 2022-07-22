d90aeefcd04ace062145508c6f0f791d3ab5caba
igorRL <igor-rl@hotmail.com>
2022-07-22 11:53:36 -0300

    validador de variaveis recebidas

--- src/instituitions/dto/create-instituition.dto.ts
+++ src/instituitions/dto/create-instituition.dto.ts
@@ -1,8 +1,21 @@
+import { IsString } from "class-validator"
+
 export class CreateInstituitionDto {
+     @IsString()
      finsfin_nome_fantasia:string
+
+     @IsString()
      finsfin_razao_social:string
+
+     @IsString()
      finsfin_code_banco:string
+
+     @IsString()
      finsfin_logo:string
+
+     @IsString()
      finsfin_cnpj:string
+
+     @IsString()
      finsfin_status:string
 }
--- src/instituitions/dto/update-instituition.dto.ts
+++ src/instituitions/dto/update-instituition.dto.ts
@@ -1,11 +1,23 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateInstituitionDto } from './create-instituition.dto';
+import { IsString } from "class-validator";
 
 export class UpdateInstituitionDto extends PartialType(CreateInstituitionDto) {
+    @IsString()
     finsfin_nome_fantasia:string
+
+    @IsString()
     finsfin_razao_social:string
+
+    @IsString()
     finsfin_code_banco:string
+
+    @IsString()
     finsfin_logo:string
+
+    @IsString()
     finsfin_cnpj:string
+
+    @IsString()
     finsfin_status:string
 }
